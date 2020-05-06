31412034678cff02bebc6af069300a818ffa2297
StoyanMStoyanov <stoyanovengineer@gmail.com>
2020-05-06 08:40:34 +0300

    Set express.js for express configuration - not full config.

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
 let express = require('express')
-let mongoose = require('mongoose')
 
 let app = express()
 
 let env = process.env.NODE_ENV || 'development'
 let config = require('./server/config/config')[env]
 
-mongoose.Promise = global.Promise
+require('./server/config/database')(config)
+require('./server/config/express')(config, app)
 
-app.set('view engine', 'pug')
-app.set('views', './server/views')
 
 app.get('/', (req, res) => {
-  mongoose.connect(config.db, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true
-    })
-  console.log('MongoDB ready!')
-
+  
   res.render('index')
 })
-
-app.use(express.static('public'))
 
 app.listen(config.port)
 console.log(`Express ready on port ${config.port}.`)
